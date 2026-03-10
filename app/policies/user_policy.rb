402cc96640949d7378a62aeb7ecb4c26c05b2913
vincent mohanna <vince.mohanna@gmail.com>
2026-03-10 16:19:04 +0100

    added authorizations and user roles with pundit

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,39 @@
+# -------------------------------------------------------
+# Controls who can do what to User records.
+# Only admins can manage users.
+# -------------------------------------------------------
+class UserPolicy < ApplicationPolicy
+  # Only admins can see the full user list
+  def index?
+    user.admin?
+  end
+
+  # Admins can view any user; regular users can only view themselves
+  def show?
+    user.admin? || record == user
+  end
+
+  # Only admins can change roles and update other users
+  def update?
+    user.admin?
+  end
+
+  def edit?
+    update?
+  end
+
+  # Only admin can destroy users
+  def destroy?
+    user.admin?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      if user.admin?
+        scope.all # Admins see all users
+      else
+        scope.where(id: user.id) # Regular users see only themselves
+      end
+    end
+  end
+end
